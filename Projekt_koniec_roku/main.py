5fff70b0619b537e3bb41acfa9894f95233a4cee
sO0phie <147306231+sO0phie@users.noreply.github.com>
2026-05-18 18:06:15 +0200

    Update main.py

--- Projekt_koniec_roku/main.py
+++ Projekt_koniec_roku/main.py
@@ -1,6 +1,7 @@
 from kod import statystyki
 from kod import swiat
 from kod.statystyki import hero
+from kod.swiat import wioska_found
 import time
 
 print("Każda historia zaczyna się od pojawienia się bohatera....")
@@ -10,23 +11,30 @@ statystyki.hero['imie'] = imie
 print(f"Znakomicie! Zatem {statystyki.hero['imie']} idz w świat!")
 
 while statystyki.hero['hp'] > 0:
-    if len(hero.get("inventory")) != 0:
-        inp = input("Czy chcesz otworzyć swój inventory? ")
-        if inp == "tak":
-            print(hero.get("inventory"))
-    kierunek = input("W jakim kierunku chcesz iść? Na p - północ, w - wschód, z - zachód czy po - południe? ")
-    if kierunek == "p":
-        statystyki.hero['Z'] += 1
-        swiat.swiat()
-    elif kierunek == "po":
-        statystyki.hero['Z'] -= 1
-        swiat.swiat()
-    elif kierunek == "w":
-        statystyki.hero['X'] -= 1
-        swiat.swiat()
-    elif kierunek == "z":
-        statystyki.hero['X'] += 1
-        swiat.swiat()
+    if wioska_found == True:
+        czynnosc = input("Co chcesz zrobić? a - podróż, b - wgląd do inventory, c - wyświetl swoje statystyki, d - odwiedź wioskę ")
+    else:
+        czynnosc = input("Co chcesz zrobić? a - podróż, b - wgląd do inventory, c - wyświetl swoje statystyki ")
+        if czynnosc == "a":
+            if wioska_found == False:
+                kierunek = input("W jakim kierunku chcesz iść? Na p - północ, w - wschód, z - zachód czy po - południe? ")
+                if kierunek == "p" or kierunek == "po" or kierunek == "w" or kierunek == "z":
+                    swiat.swiat()
+                else:
+                    print("Wpisano niepoprawny kierunek")
+                
+        elif czynnosc == "b":
+            if len(hero.get("inventory")) != 0:
+                print(hero.get("inventory"))
+            else:
+                print("Wygląda na to że nic jeszcze nie masz!")
+        elif czynnosc == "c":
+            print("="*30)
+            for k, v in hero.items():
+                time.sleep(1)
+                print(f"{k} ----- {v}")
+else:
+    print("Nie zostało tobie życia, koniec gry!")
     else:
         print("Wpisano niepoprawny kierunek")
 else:
