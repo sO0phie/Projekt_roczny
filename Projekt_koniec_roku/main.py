f4aafe41f8c5eac4ec2047bea5182e8d3ba8efc1
sO0phie <147306231+sO0phie@users.noreply.github.com>
2026-05-19 20:39:46 +0200

    Update main.py

--- Projekt_koniec_roku/main.py
+++ Projekt_koniec_roku/main.py
@@ -1,7 +1,6 @@
 from kod import statystyki
 from kod import swiat
 from kod.statystyki import hero
-from kod.swiat import wioska_found
 import time
 
 print("Każda historia zaczyna się od pojawienia się bohatera....")
@@ -11,31 +10,40 @@ statystyki.hero['imie'] = imie
 print(f"Znakomicie! Zatem {statystyki.hero['imie']} idz w świat!")
 
 while statystyki.hero['hp'] > 0:
-    if wioska_found == True:
+    if swiat.wioska_found and swiat.working_portal:
+        czynnosc = input("Co chcesz zrobić? a - podróż, b - wgląd do inventory, c - wyświetl swoje statystyki, d - odwiedź wioskę, p - portal ")
+    elif swiat.wioska_found:
         czynnosc = input("Co chcesz zrobić? a - podróż, b - wgląd do inventory, c - wyświetl swoje statystyki, d - odwiedź wioskę ")
+    elif swiat.working_portal:
+        czynnosc = input("Co chcesz zrobić? a - podróż, b - wgląd do inventory, c - wyświetl swoje statystyki, p - portal ")
     else:
         czynnosc = input("Co chcesz zrobić? a - podróż, b - wgląd do inventory, c - wyświetl swoje statystyki ")
-        if czynnosc == "a":
-            if wioska_found == False:
-                kierunek = input("W jakim kierunku chcesz iść? Na p - północ, w - wschód, z - zachód czy po - południe? ")
-                if kierunek == "p" or kierunek == "po" or kierunek == "w" or kierunek == "z":
-                    swiat.swiat()
-                else:
-                    print("Wpisano niepoprawny kierunek")
+    if czynnosc == "a":
+        kierunek = input("W jakim kierunku chcesz iść? Na p - północ, w - wschód, z - zachód czy po - południe? ")
+        if kierunek == "p" or kierunek == "po" or kierunek == "w" or kierunek == "z":
+            swiat.swiat()
+        else:
+            print("Wpisano niepoprawny kierunek")
                 
-        elif czynnosc == "b":
-            if len(hero.get("inventory")) != 0:
-                print(hero.get("inventory"))
-            else:
-                print("Wygląda na to że nic jeszcze nie masz!")
-        elif czynnosc == "c":
-            print("="*30)
-            for k, v in hero.items():
-                time.sleep(1)
-                print(f"{k} ----- {v}")
-else:
-    print("Nie zostało tobie życia, koniec gry!")
-    else:
-        print("Wpisano niepoprawny kierunek")
+    elif czynnosc == "b":
+        if len(hero.get("inventory")) != 0:
+            print(hero.get("inventory"))
+        else:
+            print("Wygląda na to że nic jeszcze nie masz!")
+    elif czynnosc == "c":
+        print("="*30)
+        for k, v in hero.items():
+            time.sleep(1)
+            print(f"{k} ----- {v}")
+    elif czynnosc == "d":
+        if swiat.wioska_found == True and swiat.wioska is not None:
+            swiat.wioska.use()
+        else:
+            print("Nie ma wioski do odwiedzenia.")
+    elif czynnosc == "p":
+        if swiat.working_portal == True and swiat.portal is not None:
+            swiat.portal.functionable()
+        else:
+            print("Portal nie jest dostępny.")
 else:
     print("Nie zostało tobie życia, koniec gry!")
